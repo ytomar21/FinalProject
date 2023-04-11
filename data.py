d372d7346cf569979a41d42459d5d806d61189be
Yajur21 <yajur.tomar@gmail.com>
2023-04-11 13:03:24 -0400

    Initial Commit

---
+++ data.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+
+
+'''
+ data_loader: Used to Prepare Data For MLP in a dataframe format
+ 
+    Inputs:
+        file (str): CSV file of dataset provided by Building Performance Dataset
+        encoder (Encoder): used to encode all categorical variables
+        label (str): Type of EUI used as label
+    
+    Output:
+        df (DataFrame): data formatted and cleaned in DataFrame
+        labels (DataFrame): labels of data
+
+'''
+def data_loader(file, encoder, label):
+
+    # Import Dataset
+    df = pd.read_csv(file, na_values=['Unknown', 'No Value'])
+    df = df.drop([])
+
+    # Separate Labels and Categorical Features
+    labels = df[label]
+    categorical_feats = ['year', 'zip_code', 'city', 'state', 'climate', 'building_class', 'facility_type', 'year_built',
+                         'operating_hours','lighting','air_flow_control','heating','heating_fuel','cooling','wall_type',
+                         'wall_insulation_r_value','roof_ceiling']
+
+    #Encode all Categorical Features
+    df[categorical_feats] = df[categorical_feats].apply(encoder.fit_transform)
+
+    #Replace all "Unknown" and "No Value" into -1
+    df = df.fillna(-1)
+    labels = labels.fillna(-1)
+
+    df = df.iloc[:, :-6]
+
+    return df, labels
+
+
+'''
+ data_visualization: Used to Visualize Data
+
+    Inputs:
+        file (str): CSV file of dataset provided by Building Performance Dataset
+
+    Output:
+        None
+
+'''
+def data_visualization(file):
+
+    df = pd.read_csv(file, na_values=['Unknown', 'No Value'])
+    print(f'floor_area: {df["floor_area"]}')
+    print(f'site_eui: {df["site_eui"]}')
+
+    df.plot.scatter(x="site_eui", y="floor_area")
+    plt.show()
+
+
+
+
+
+#Initialize Variables for Data Loading
+file = "Data/Los Angeles Benchmarking Ordinance.csv"
+encoder = LabelEncoder()
+label = "site_eui"
+
+df, labels = data_loader(file, encoder, label)
+
+#Split dataset into train and test
+X_train, X_test, y_train, y_test = train_test_split(df, labels, test_size=0.2)
+
+#print(f'X_train: {X_train}')
+#print(f'y_train: {y_train}')
+
+#Train simple MLP (Multi layer perceptron) Regression
+model = MLPRegressor(max_iter=500).fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+y_pred = np.array(y_pred)
+y_test = np.array(y_test)
+
+print(f'y_pred: {y_pred}')
+print(f'y_test: {y_test}')
+
+#Evaluate Results
+print(f'R^2 Score: {r2_score(y_test, y_pred)}')
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from data import data_visualization
+
+file = "Data/Philadelphia Benchmarking Ordinance.csv"
+data_visualization(file)
